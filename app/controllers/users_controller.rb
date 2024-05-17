10d8f3f54fdac12812df6348a404d86f0dd20a2c
Sara-Alhaddadi <sara.khalid.alhaddadi@gmail.com>
2024-05-17 16:00:17 +0300

    fix UI and add active storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,6 +65,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password_digest, :role)
+      params.require(:user).permit(:name, :email, :password_digest, :role, :avatar)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
+  has_one_attached :avatar
+  enum role: { student: 0, teacher: 1, admin: 2 }
+
   has_many :courses, foreign_key: :instructor_id
   has_many :enrollments
   has_many :enrolled_courses, through: :enrollments, source: :course
+
+  def avatar_image
+    if avatar.attached?
+      avatar
+    else
+      'default-avatar.png'
+    end
+  end
 end
